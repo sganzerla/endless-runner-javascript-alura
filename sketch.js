46ab7682c012d6df83a993d0ec1c7e3ba0f26f53
alisson sganzerla <alisson_sganzerla@yahoo.com.br>
2020-06-22 20:32:46 -0300

    add cenario e personagem

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let imagemCenario;
+let imagemPersonagem;
+
+function preload(){
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(imagemCenario); 
+  
+  image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270);
 }
